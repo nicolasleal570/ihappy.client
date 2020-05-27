e240ed16be6244828fed813db903488f561443f2
juanchete <juanlopezlmg@gmail.com>
2020-05-27 17:41:00 -0400

    sign-up pero desalineada ligeramente y sin funcion

---
+++ pages/sign-up.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signup from '../components/public/sing-up/signup'
+
+export default function sign_up() {
+    return (
+        <div>
+            <Signup/>
+        </div>
+    )
+}
