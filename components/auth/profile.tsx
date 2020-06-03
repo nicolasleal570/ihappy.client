986997e0822268b663878674f7185bba33e2d7af
Gianluca Di Bella <gianluca.dib99@gmail.com>
2020-06-03 01:49:40 -0400

    Listo

--- components/auth/profile.tsx
+++ components/auth/profile.tsx
@@ -58,15 +58,13 @@ export default function Profile() {
             }
         }
         axios.put('http://localhost:5000/api/users/avatar', formData, config).then((res) => {
-                console.log(res.data);
-                dispatch(updateUser(res.data.data))
-            })
+            console.log(res.data);
+            dispatch(updateUser(res.data.data))
+        })
             .catch((err) => {
                 console.log(err);
             })
     }
-
-
 
     const obtenerDatos = async () => {
         const res = await axios.get('http://localhost:5000/api/users', {
@@ -88,31 +86,31 @@ export default function Profile() {
     const onHandle = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         let formdata = new FormData()
-        formdata.append('image', archivo as any)        
+        formdata.append('image', archivo as any)
         enviarFoto(formdata);
     }
 
     return (
-        <div className='flex bg-gray-300 h-screen'>
+        <div className='flex bg-cover bg-top bg-no-repeat h-screen' style={{backgroundImage: `url('/assets/img/chill2.png')`}}>
             <div className='w-1/5'>
             </div>
-            <div className='flex w-5/5 ml-20 mt-10'>
+            <div className='w-5/5 ml-20 mt-10'>
                 <form method="PUT" onSubmit={onSubmit} className='ml-10 mt-5'>
-                    <h1 className="font-bold capitalize text-xl text-center py-4">Account Overview</h1>
+                    <h1 className="font-bold capitalize text-xl text-center text-white mr-16 py-4">Account Overview</h1>
                     <div className='flex'>
                         <div className=''>
                             <div className="w-full py-1 md:w-1/2 px-2 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                                <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-name">
                                     First Name </label>
-                                <input id='Nombre' onChange={e => setNombre(e.target.value)} value={nombre} size={50} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" />
+                                <input id='Nombre' onChange={e => setNombre(e.target.value)} value={nombre} size={80} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" />
                             </div>
                             <div className="w-full py-1 md:w-1/2 px-2">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                                <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-last-name">
                                     Last Name </label>
                                 <input id='Apellido' onChange={e => setApellido(e.target.value)} value={apellido} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder={apellido} />
                             </div>
                             <div className="w-full py-1 md:w-1/2 px-2 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-cedula">
+                                <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-cedula">
                                     Cedula </label>
                                 <input id='cedula' onChange={e => setCedula(e.target.value)} value={cedula} size={20} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="number" placeholder={cedula} />
                             </div>
@@ -122,10 +120,10 @@ export default function Profile() {
                             <input id='avatar' onChange={e => setArchivo(e.target.files![0])} size={20} className=""  type="file" />
                         </div> */}
                             <div className="w-full py-1 md:w-1/2 px-2 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-cedula">
+                                <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-cedula">
                                     Direccion </label>
                                 {/* <input id='direccion' onChange={e => setDireccion(e.target.value)} value={direccion} size={20} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 mb-3 px-2 leading-tight focus:outline-none focus:bg-white"  type="text" placeholder={direccion} /> */}
-                                <textarea id='direccion' onChange={e => setDireccion(e.target.value)} value={direccion} className="flex inline-block p-2 transition h-50 bg-gray-200 h-40 ease-in-out bg-transparent border-2 border-purple-600 hover:bg-transparent hover:border-purple-800 rounded" placeholder={direccion}></textarea>
+                                <textarea id='direccion' onChange={e => setDireccion(e.target.value)} value={direccion} className="appearance-none block w-full h-32 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" placeholder={direccion}></textarea>
                             </div>
                             {/* <div className="ml-2 mr-56 py-1">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-cedula">Role</label>
@@ -148,24 +146,23 @@ export default function Profile() {
                         </div> */}
                         </div>
                         <div className="w-full md:w-1/2 py-3 px-2 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-bio">
+                            <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-first-bio">
                                 Biografia </label>
-                            <textarea id='Biografia' onChange={e => setBiografia(e.target.value)} value={biografia} className="flex inline-block p-2 transition h-full ease-in-out bg-transparent border-2 border-purple-600 hover:bg-transparent hover:border-purple-800 rounded" placeholder={biografia}></textarea>
+                            <textarea id='Biografia' onChange={e => setBiografia(e.target.value)} value={biografia} className="appearance-none block w-full h-64 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" placeholder={biografia}></textarea>
+                            <button type='submit' className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4  rounded-full">Guardar Datos</button>
                         </div>
-
-                    </div>
-                    <div className='mt-10 ml-64'>
-                        <button type='submit' className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full">Guardar</button>
-                    </div>
+                    
+                    
+                </div>
                 </form>
-                <div className='mt-40'>
+                <div className=''>
                     <form encType='multipart/form-data' onSubmit={onHandle} method="PUT">
                         <div className="w-full py-1 md:w-1/2 px-2 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-imagen">
+                            <label className="block uppercase tracking-wide text-white text-xs ml-10 font-bold mb-2" htmlFor="grid-first-imagen">
                                 Carga tu avatar </label>
-                            <input id='image' onChange={e => setArchivo(e.target.files![0])} size={20} className="" type="file" />
+                            <input id='image' onChange={e => setArchivo(e.target.files![0])} size={20} className="ml-10" type="file" />
                         </div>
-                        <button type='submit' className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 mt-2 ml-2 px-4 rounded-full">Guardar foto</button>
+                        <button type='submit' className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 mt-2 ml-12 px-4 rounded-full">Guardar foto</button>
                     </form>
                 </div>
             </div>
