1529f99e5606c97e9add2849c6df60637fa05ffc
nicolasleal570 <lealnicolas96@gmail.com>
2020-06-05 03:53:07 -0400

    swDest bug fixed

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withOffline = require('next-offline');
 
 const nextConfig = {
     workboxOpts: {
-        swDest: '/public/service-worker.js',
+        swDest: 'static/service-worker.js',
         runtimeCaching: [
             {
                 urlPattern: /^https?.*/,
