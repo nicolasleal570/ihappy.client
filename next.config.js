efb0580391b1ac76cdcf288ebafa5723ac6e69b8
nicolasleal570 <lealnicolas96@gmail.com>
2020-06-05 03:41:46 -0400

    Bug fixed service worker

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withOffline = require('next-offline');
 
 const nextConfig = {
     workboxOpts: {
-        swDest: 'static/service-worker.js',
+        swDest: '/public/static/service-worker.js',
         runtimeCaching: [
             {
                 urlPattern: /^https?.*/,
