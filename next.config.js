cc1b33545630607dcc796a295392daea9fa8d6dd
nicolasleal570 <lealnicolas96@gmail.com>
2020-06-06 17:01:02 -0400

    next offline adding scope var

--- next.config.js
+++ next.config.js
@@ -19,7 +19,8 @@ const nextConfig = {
                     }
                 }
             }
-        ]
+        ],
+        "scope": "/app/"
     },
     reactStrictMode: true,
     experimental: {
