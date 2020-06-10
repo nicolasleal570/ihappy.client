2bf078fe16aeab380384aa7aed4651f4e794b3f4
Gianluca Di Bella <gianluca.dib99@gmail.com>
2020-06-10 11:57:12 -0400

    faq-fix

--- pages/faq-contact.tsx
+++ pages/faq-contact.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PublicLayout from '../components/public/Layout';
-import Faq from '../components/public/FAQ'
+import Faq from '../components/public/faq-contact'
 
-const FAQ = () => {
+const faqContact = () => {
     return (
         <PublicLayout>
             <Faq />
         </PublicLayout>
     )
 }
-export default FAQ
+export default faqContact
