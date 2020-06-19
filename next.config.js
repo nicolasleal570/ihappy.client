47507a6ae9b1533b5bf0a111aec77193305e89bc
nicolasleal570 <lealnicolas96@gmail.com>
2020-06-18 23:43:25 -0400

    Adding extra config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,30 +1,34 @@
 const withOffline = require('next-offline');
 
 const nextConfig = {
-    workboxOpts: {
-        swDest: 'static/service-worker.js',
-        runtimeCaching: [
-            {
-                urlPattern: /^https?.*/,
-                handler: 'NetworkFirst',
-                options: {
-                    cacheName: 'https-calls',
-                    networkTimeoutSeconds: 15,
-                    expiration: {
-                        maxEntries: 150,
-                        maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-                    },
-                    cacheableResponse: {
-                        statuses: [0, 200]
-                    }
-                }
-            }
-        ],
-    },
-    reactStrictMode: true,
-    experimental: {
-        modern: true
-    }
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /\.(?:png|jpg|jpeg|svg)/,
+        handler: 'CacheFirst',
+      },
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+  reactStrictMode: true,
+  experimental: {
+    modern: true,
+  },
 };
 
 module.exports = withOffline(nextConfig);
