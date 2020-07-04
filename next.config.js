b5a68f3df92eec141eb17d16be565a53fe78ee1b
nicolasleal570 <lealnicolas96@gmail.com>
2020-07-04 03:17:28 -0400

    Change next-offline to next-pwa

--- next.config.js
+++ next.config.js
@@ -1,30 +1,8 @@
-const withOffline = require('next-offline');
+const withPWA = require('next-pwa')
 
 const nextConfig = {
-    workboxOpts: {
-        swDest: 'static/service-worker.js',
-        runtimeCaching: [
-            {
-                urlPattern: /^https?.*/,
-                handler: 'NetworkFirst',
-                options: {
-                    cacheName: 'https-calls',
-                    networkTimeoutSeconds: 15,
-                    expiration: {
-                        maxEntries: 150,
-                        maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-                    },
-                    cacheableResponse: {
-                        statuses: [0, 200]
-                    }
-                }
-            }
-        ],
-    },
-    reactStrictMode: true,
-    experimental: {
-        modern: true
+    pwa: {
+      dest: 'public'
     }
-};
-
-module.exports = withOffline(nextConfig);
+  }
+module.exports = withPWA(nextConfig);
