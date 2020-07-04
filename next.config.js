de58b7d52e8206130982aa6fe9fb470eb5c3832e
nicolasleal570 <lealnicolas96@gmail.com>
2020-07-04 03:43:54 -0400

    Scope added to next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withPWA = require('next-pwa')
 
 const nextConfig = {
     pwa: {
+      disable: process.env.NODE_ENV === 'development',
+      register: true,
+      scope: '/app',
+      sw: 'service-worker.js',
       dest: 'public'
     }
   }
