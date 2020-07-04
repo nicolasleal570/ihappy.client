282e0f52a8d64f670c913fe8c0f2ce69d3e3ea9e
nicolasleal570 <lealnicolas96@gmail.com>
2020-07-04 13:04:56 -0400

    Bug fixed in scope

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa');
 
 const nextConfig = {
-    pwa: {
-      disable: process.env.NODE_ENV === 'development',
-      register: true,
-      scope: '/app',
-      sw: 'service-worker.js',
-      dest: 'public'
-    }
-  }
+  pwa: {
+    scope: '/',
+    dest: 'public',
+  },
+};
 module.exports = withPWA(nextConfig);
