05317a0d4b96c4cc93b2dbfacd5c9424833ba61d
nicolasleal570 <lealnicolas96@gmail.com>
2020-07-05 18:53:10 -0400

    Better performance on images and tailwind.config.js

--- components/public/login/login.tsx
+++ components/public/login/login.tsx
@@ -83,7 +83,7 @@ export default function login() {
       <div
         className="flex-1 bg-center bg-no-repeat bg-cover h-screen"
         style={{
-          backgroundImage: `url('/assets/img/login-image.webp')`,
+          backgroundImage: `url('/assets/img/login-image.jpg')`,
         }}
       >
         <div className="flex justify-center h-full">
