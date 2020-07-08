6a9a3191156f9fdff90c51ba9b7ad2901379aa77
nicolasleal570 <lealnicolas96@gmail.com>
2020-07-08 13:52:05 -0400

    bug fixed

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@ const withPWA = require('next-pwa');
 
 const nextConfig = {
   pwa: {
-    scope: '/',
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
   },
 };
 module.exports = withPWA(nextConfig);
