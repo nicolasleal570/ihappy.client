fc5fb0fc91ef2e3878ee8c775d35173c80c6b6c3
nicolasleal570 <lealnicolas96@gmail.com>
2020-07-08 00:36:10 -0400

    Tailwind config and merge develop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { fontFamily } = require('tailwindcss/defaultTheme');
 
 // tailwind.config.js
 module.exports = {
