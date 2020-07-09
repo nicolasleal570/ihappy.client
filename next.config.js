21c15e8456067715a11e2f18b66d216b11870fbd
nicolasleal570 <lealnicolas96@gmail.com>
2020-07-08 21:09:28 -0400

    Bug fixed in now.json config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@ const withPWA = require('next-pwa');
 
 const nextConfig = {
   pwa: {
-    swDest: 'static/service-worker.js',
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
     register: true,
-    sw: '/service-worker.js'
+    sw: '/sw.js'
   },
 };
 module.exports = withPWA(nextConfig);
