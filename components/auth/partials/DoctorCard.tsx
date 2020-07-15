a120b8d5b55938da6a59d3c7631718bb411ba426
Gianluca Di Bella <gianluca.dib99@gmail.com>
2020-07-15 11:31:12 -0400

    doctor card Fix

--- components/auth/partials/DoctorCard.tsx
+++ components/auth/partials/DoctorCard.tsx
@@ -38,7 +38,7 @@ const DoctorCard = ({
 
             {/* Specialities */}
             {specialities && (
-            <div className="flex items-start justify-center flex-col px-4 my-4">
+            <div className="flex items-start justify-center flex-col px-4 my-4 pb-5">
 
                 {specialities.map((elemen) => (
                     <span key={elemen.slug} className="px-2 py-1 text-xs bg-purple-200 mb-2 rounded capitalize border border-purple-300">{elemen.name}</span>
@@ -52,7 +52,7 @@ const DoctorCard = ({
             </div>}
 
             {/* Btn view profile */}
-            <div className="w-full bg-gray-100 flex items-center justify-center py-2 border-t border-gray-300 px-4">
+            <div className="w-full absolute bottom-0 bg-gray-100 flex items-center justify-center py-1 border-t border-gray-300 px-4">
                 <Link href="/reviews/[slug]" as={`/reviews/${slug}`}>
                     <a className="text-center px-3 py-1 text-sm bg-purple-700 rounded text-white">Ver Perfil</a>
                 </Link>
